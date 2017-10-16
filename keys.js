d7420fae92cc8e98dfe83ef8079e3940d372de1f
Jachaplin <jakealanchaplin@yahoo.com>
2017-10-16 15:44:48 -0700

    initial files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'STXXzg1NgKR2OrBFci5v263Zn',
+  consumer_secret: 'dobhIoREGisLmMXpI02zJ8aBlcA07iVUEJPVBU97sQUyZCOggK',
+  access_token_key: '919855136951775232-u68HadQ0uB67gJZQefgzgw6RVAsaB0v',
+  access_token_secret: 'eZspcKUNeEK483EDK533zslF8yoqguBVL6f9TZUwUqIF3',
+}
+
+module.exports = twitterKeys;
